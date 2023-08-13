61e572838e34423b944839435a98cc8f5e96ea78
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-08-13 15:49:20 +0530

    Add files via upload

--- reactangle.cpp
+++ reactangle.cpp
@@ -42,3 +42,54 @@ int main(){
     
     return 0;
 }
+
+
+
+
+//this code is modified code by using loop we are creating the object 
+
+// #include<iostream>
+// using namespace std;
+
+// class rectrangle{
+//   private:
+//          int len,wid,area;
+//          static int count;
+         
+//   public:
+//         void getdata(void){
+//             cout<<"enter the value of length and width"<<endl;
+//             cin>>len>>wid;
+//             count++;
+//         }
+        
+//         void areaofrectrangle(void){
+//             area = len*wid;
+//             cout<<"the area of rectrangle is "<<area<<endl;
+//             }
+        
+//         static void getcount(){
+//             cout<<"the user find area of rectrangle  "<<count<<"  times"<<endl;
+//         }
+        
+// };
+
+// int rectrangle :: count;
+
+// int main(){
+    
+//     rectrangle ob[5];
+    
+// for(int i=0; i<5; i++){
+//     ob[i].getdata();
+//     ob[i].areaofrectrangle();
+// }
+    
+   
+
+//     rectrangle::getcount();
+    
+    
+    
+//     return 0;
+// }
