3280bb62a0b32fdc382d331ed9b0ed616fa3db4b
ankit ranjan <110593132+Ankitrj3@users.noreply.github.com>
2023-08-17 17:16:42 +0530

    Add files via upload

---
+++ array1.js
@@ -0,0 +1,15 @@
+// array is mutable
+// array is object
+
+let marks = [91,23,55,67,45,"not present"];
+console.log(marks);
+console.log(marks[0]);
+console.log(marks[1]);
+console.log(marks[2]);
+console.log(marks[3]);
+console.log(marks[5]);
+console.log("the length of  "+marks.length);
+
+marks[5]=89; //changing the value of the array
+
+console.log(marks[5]);
